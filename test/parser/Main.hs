ea0d85d71f8f9c2dec9df9836e8d1b71184a2a9d
Jamie Willis <j.willis19@imperial.ac.uk>
2021-08-20 17:26:29 +0100

    Added parser tests

--- test/parser/Main.hs
+++ test/parser/Main.hs
@@ -1,4 +1,17 @@
 module Main where
 
+import Arbitrary
+import qualified WeakAST as Weak
+import qualified StrongAST as Strong
+
+import Test.Tasty
+import Test.Tasty.QuickCheck
+import Pretty
+import Parser
+
 main :: IO ()
-main = return ()
+main = defaultMain $ testGroup "Conversions" [
+    --testProperty "parse . pretty = id" roundtrip
+  ]
+
+roundtrip e = parseExpr (pretty e) === e
