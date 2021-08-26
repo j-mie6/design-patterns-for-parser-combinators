677a1ec015b6ec9daad1e680b8892b71b607fc4d
Nicolas Wu <n.wu@imperial.ac.uk>
2021-08-26 15:38:39 +0100

    Fixed whitespace parsing issue (Patterns 2a and 2bi)
    ```
    {---------------------------------------------}
    {-     Pattern 2a: Whitespace Combinators    -}
    {---------------------------------------------}
    {- Build a `lexeme` combinator dedicated to  -}
    {- consuming trailing whitespace after a     -}
    {- given lexeme. Build a `fully` combinator  -}
    {- to consume initial whitespace and the end -}
    {- of input.                                 -}
    {---------------------------------------------}
    ```
    
    After factoring the tokens into `Lexer.hs`, building the
    new combinators helps separate individual responsibilities.
    
    ```
    {---------------------------------------------}
    {-    Pattern 2bi: Tokenising Combinators    -}
    {---------------------------------------------}
    {- Annotate terminals with a `token`         -}
    {- combinator, built on `lexeme`, to         -}
    {- atomically parse them with whitespace     -}
    {- consumed.                                 -}
    {---------------------------------------------}
    ```
    
    Building a `token` combinator helps keep things atomic.

--- src/Parser.hs
+++ src/Parser.hs
@@ -4,6 +4,7 @@ import Prelude hiding (negate)
 
 import WeakAST
 import Miniparsec
+import Lexer
 
 import Data.Char (digitToInt)
 
@@ -11,39 +12,19 @@ import Data.Char (digitToInt)
 This function should turn a string into an expression.
 -}
 parseExpr :: String -> Either String Expr
-parseExpr = parse expr
-
-alpha :: Parser Char
-alpha = oneOf (['a' .. 'z'] ++ ['A' .. 'Z'])
-    <?> "letter"
-digit :: Parser Char
-digit = oneOf ['0' .. '9']
-    <?> "digit"
-alphaNum :: Parser Char
-alphaNum = alpha <|> digit
-
-number :: Parser Int
-number = foldl addDigit 0 <$> some digit
-     <?> "number"
-
-ident :: Parser [Char]
-ident = alpha <:> many alphaNum
-    <?> "identifier"
-
-addDigit :: Int -> Char -> Int
-addDigit n d = n * 10 + digitToInt d
+parseExpr = parse (fully expr)
 
 expr :: Parser Expr
-expr = chainr1 term (Add <$ char '+' <|> Sub <$ char '-')
+expr = chainr1 term (Add <$ token (char '+') <|> Sub <$ token (char '-'))
 
 term :: Parser Expr
-term = chainr1 negate (Mul <$ char '*')
+term = chainr1 negate (Mul <$ token (char '*'))
 
 negate :: Parser Expr
-negate = Neg <$> (string "negate" *> negate)
+negate = Neg <$> (token (string "negate") *> negate)
      <|> atom
 
 atom :: Parser Expr
-atom = char '(' *> expr <* char ')'
+atom = token (char '(') *> expr <* token (char ')')
    <|> Num <$> number
    <|> Var <$> ident
